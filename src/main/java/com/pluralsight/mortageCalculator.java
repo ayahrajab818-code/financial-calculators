3e435af134115d4eb33e6849a6eeb05f150c5e63
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-09-29 16:37:59 -0400

    mortgage calculator

---
+++ src/main/java/com/pluralsight/mortageCalculator.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class mortageCalculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the user input
+        System.out.print("Enter the principal loan: ");
+        double principle = scanner.nextDouble();
+
+        System.out.print("Enter the annual interest rate: ");
+        double annualRate = scanner.nextDouble();
+
+        System.out.print("Enter the number of years for the loan: ");
+        int years = scanner.nextInt();
+
+        //Convert to decimal
+        double i = annualRate / 100 / 12;
+        int n = years * 12;
+
+
+        //Calculate monthly rate
+        double monthlyPayment = principle * (i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
+
+        //Calculate total interest
+        double totalInterest = (monthlyPayment * n ) - principle;
+
+        //Result
+        System.out.printf("Monthly Payment: $%.2f%n", monthlyPayment);
+        System.out.printf("Total interest: $%.2f%n" , totalInterest);
+    }
+}
